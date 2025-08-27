8d0a233331fdd6f51f507ca3802e85121e47efc1
skycode9 <skycode9005@gmail.com>
2025-08-27 14:28:43 -0600

    this basic cheack of authorization

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/middleware", (req, res, next) => {
-  console.log("Route Handler");
-  next();
+app.get("/admin/getAllData", (req, res) => {
+  // need to check is admin is logged in or not
+  const token = "Admin";
+  const isAdmin = token === "Admin";
+  if (!isAdmin) {
+    return res.status(401).send("You are not authorized");
+  }
+  res.send("Get All Data");
 });
-
-app.get("/middleware", (req, res) => {
-  console.log("Route Handler 2");
-  res.send("Route Handeler 2");
+app.delete("/admin/deleteData", (req, res) => {
+  // need to check is admin is logged in or not
+  const token = "Admin1";
+  const isAuthorized = token === "Admin";
+  if (!isAuthorized) {
+    return res.status(401).send("Your are not authorized");
+  }
+  res.send("Delete Data");
 });
-
 app.listen("3030", () => {
   console.log("server running on 3030");
 });

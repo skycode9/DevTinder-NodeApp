dcd70a1145db369d8de36ec09764e49658ecf0b6
skycode9 <skycode9005@gmail.com>
2025-08-27 14:31:35 -0600

    now make a separate middleware function in the app.js for "/admin" routes

--- src/app.js
+++ src/app.js
@@ -2,22 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/admin/getAllData", (req, res) => {
-  // need to check is admin is logged in or not
+app.use("/admin", (req, res, next) => {
+  console.log("Admin routes check every time to go Admin sites");
   const token = "Admin";
-  const isAdmin = token === "Admin";
-  if (!isAdmin) {
-    return res.status(401).send("You are not authorized");
-  }
-  res.send("Get All Data");
-});
-app.delete("/admin/deleteData", (req, res) => {
-  // need to check is admin is logged in or not
-  const token = "Admin1";
   const isAuthorized = token === "Admin";
   if (!isAuthorized) {
     return res.status(401).send("Your are not authorized");
   }
+  next();
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("Get All Data");
+});
+app.delete("/admin/deleteData", (req, res) => {
   res.send("Delete Data");
 });
 app.listen("3030", () => {

4b04b63a6362d7c2eeb23e378cbef740c04e872d
skycode9 <skycode9005@gmail.com>
2025-08-27 14:08:19 -0600

    making a totally diffrent function which will working same  when we use next fn

--- src/app.js
+++ src/app.js
@@ -2,18 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/middleware",
-  (req, res, next) => {
-    console.log("Route Handler");
-    next();
-    res.send("Route Handler 1");
-  },
-  (req, res) => {
-    console.log("Route Handler 2");
-    res.send("Route Handler 2");
-  }
-);
+app.get("/middleware", (req, res, next) => {
+  console.log("Route Handler");
+  next();
+});
+
+app.get("/middleware", (req, res) => {
+  console.log("Route Handler 2");
+  res.send("Route Handeler 2");
+});
 
 app.listen("3030", () => {
   console.log("server running on 3030");

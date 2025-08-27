5d6ae864bae5f1d1341e938b4adf9b988265f8f3
skycode9 <skycode9005@gmail.com>
2025-08-27 14:55:36 -0600

    this one is not caching the error because of oder of routes is matter is above the all routes

--- src/app.js
+++ src/app.js
@@ -2,15 +2,15 @@ const express = require("express");
 const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
+// order of routes is matter
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.send("Something went wrong");
+});
 
 app.get("/error", (req, res) => {
   throw new Error("Something went wrong");
   res.send("Error");
-});
-
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.send("Something went wrong");
 });
 
 app.listen("3030", () => {
